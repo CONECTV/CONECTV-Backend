0255977c4feb8c420518b642799892ad6762f156
RafaGomezGlez <A01733983@itesm.mx>
2022-06-21 14:18:44 -0500

    feat(index): General express configuration CON-21

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const sequelize = require('./src/database/configs/db_connection')
 const hostname = '127.0.0.1';
-const port = process.env.PORT || 8080;
+const port = 8000;
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express()
+const userRoutes = require('./src/routes/users')
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use('/users', userRoutes);
+
 
 app.listen(port, () => {
   console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
 });
-
-
 var models = [
   'user',
   'technicalServiceStatus',
