bc363f6cd449ac87a4b6fc3c01b82a070282e47c
Rafael Gómez <61569733+RafaGomezGlez@users.noreply.github.com>
2022-06-22 17:21:46 -0500

    Revert "Feat/internet tarifs con 29"

--- src/models/statusTechnicalService.js
+++ src/models/statusTechnicalService.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 
 const sequelize = require('../configs/db_connection');
+const AccountStatus = require('./accountStatus');
 const SuscriptorData = require('./suscriptorData');
 const TechnicalServices = require('./technicalServices');
 const TechnicalServiceStatus = require('./technicalServiceStatus');
@@ -12,9 +13,33 @@ const TechnicalServicesHistory = sequelize.define('technicalServicesHistory', {
         allowNull: false,
         primaryKey: true
     },
+    idSuscriptor: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+            model: SuscriptorData,
+            key: 'id'
+        }
+    },
+    idTechnicalService:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+            model: TechnicalServices,
+            key: 'id'
+        }
+    },
     commentary: {
         type: Sequelize.TEXT,
         allowNull: false
+    },
+    statusTechnicalService: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+            model: TechnicalServiceStatus,
+            key: 'id'
+        }
     },
     startDate:{
         type: Sequelize.DATE,
@@ -24,15 +49,5 @@ const TechnicalServicesHistory = sequelize.define('technicalServicesHistory', {
         type: Sequelize.DATE,
     }
 });
-
-
-SuscriptorData.hasMany(TechnicalServicesHistory);
-TechnicalServicesHistory.belongsTo(SuscriptorData);
-
-TechnicalServicesHistory.hasMany(TechnicalServices)
-TechnicalServices.belongsTo(TechnicalServicesHistory)
-
-TechnicalServicesHistory.hasMany(TechnicalServiceStatus)
-TechnicalServiceStatus.belongsTo(TechnicalServicesHistory)
 
 module.exports = TechnicalServicesHistory;
--- src/routes/login.js
+++ src/routes/login.js
@@ -4,6 +4,7 @@ import { getLogin, postLogin } from '../controllers/login.js';
 
 const router = express.Router();
 
+//localhost:1010/login
 router.get('/prueba', getLogin)
 router.post('/insertar', postLogin)
 
