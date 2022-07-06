575e8d9a6d21dde7a8b4f4b3d3e6421dc9349d8a
RafaGomezGlez <A01733983@itesm.mx>
2022-07-06 13:51:33 -0500

    feat(instalationData): PUT endpoint created CON-67

--- src/controllers/instalationData.js
+++ src/controllers/instalationData.js
@@ -16,7 +16,7 @@ exports.createInstalationData =  async (req, res) => {
         )
         res.status(201).send(`instalationData with IP Adreess ${req.body.ipAddress} was successfully created`);
     } catch (error) {
-        console.log(error.message)
+        res.status(400).json({error})
     }
 }
 
@@ -25,6 +25,32 @@ exports.getInstalationData  =  async (req, res) => {
         const instalationsData = await InstalationData.findAll();
         res.status(200).send(JSON.stringify(instalationsData));
     } catch (error) {
-        console.log(error.message)
+        res.status(400).json({error})
     }
 };
+
+exports.updateInstalationData = async (req, res) => {
+    try {
+        const isInstalationDataExists =  await InstalationData.findOne({ where: { id: Number(req.body.id) }})
+        if (!isInstalationDataExists) return res.status(400).json({error: 'InstalationData not found'})
+
+        await InstalationData.update({ 
+            instalationDate: new Date(req.body.instalationDate),
+            poleNumber: Number(req.body.poleNumber),
+            napNumber: Number(req.body.napNumber),
+            napOutput: Number(req.body.napOutput),
+            macAddress: req.body.macAddress,
+            ipAddress: req.body.ipAddress,
+            numberTelevisions: Number(req.body.numberTelevisions)
+        },{
+            where: { 
+                id: Number(req.body.id)
+            }
+        })
+
+        res.status(200).send(`instalationData with IP Adreess ${req.body.ipAddress} was successfully updated`);
+
+    } catch (error) {
+        res.status(400).json({error})
+    }
+}
