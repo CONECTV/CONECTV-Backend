e0ed6d8ccc241b8d0452a73445cd10ce2a6b4ef1
RafaGomezGlez <A01733983@itesm.mx>
2022-07-06 11:47:24 -0500

    feat(accountStatus): Account status UPDATE endpoint CON-55

--- src/routes/accountStatus.js
+++ src/routes/accountStatus.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const createAccountStatus = require('../controllers/accountStatus');
 const getAccountStatuses = require('../controllers/accountStatus');
+const updateAccountStatus = require('../controllers/accountStatus');
 
 const router = express.Router();
 
 router.post('/crear', createAccountStatus.createAccountStatus);
 router.get('/obtener', getAccountStatuses.getAccountStatuses);
+router.put('/actualizar', updateAccountStatus.updateAccountStatus);
 
 module.exports = router;
