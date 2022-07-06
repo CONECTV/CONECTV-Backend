6433a0faf8e9cce564da336284fbe9e7017df90c
RafaGomezGlez <A01733983@itesm.mx>
2022-07-06 13:35:10 -0500

    feat(technicalServiceStatus): Endpoint PUT created CON-64

--- src/controllers/technicalServiceStatus.js
+++ src/controllers/technicalServiceStatus.js
@@ -10,7 +10,7 @@ exports.createTechnicalServiceStatus =  async (req, res) => {
         )
         res.status(201).send(`Technical Services Status with status ${req.body.status} was successfully created`);
     } catch (error) {
-        console.log(error.message)
+        res.status(400).json({error})
     }
 }
 
@@ -19,6 +19,26 @@ exports.getTechnicalServicesStatus =  async (req, res) => {
         const technicalServiceStatus = await TechnicalServiceStatus.findAll();
         res.status(200).send(JSON.stringify(technicalServiceStatus));
     } catch (error) {
-        console.log(error.message)
+        res.status(400).json({error})
     }
 };
+
+exports.updateTechnicalServiceStatus = async (req, res) => {
+    try {
+        const isTechnicalServiceStatusExists =  await TechnicalServiceStatus.findOne({ where: { id: Number(req.body.id) }})
+        if (!isTechnicalServiceStatusExists) return res.status(400).json({error: 'TechnicalServiceStatus not found'})
+
+        await TechnicalServiceStatus.update({ 
+            status: req.body.status
+        },{
+            where: { 
+                id: Number(req.body.id)
+            }
+        })
+
+        res.status(200).send(`Technical Service Status ${req.body.status} was successfully updated`);
+
+    } catch (error) {
+        res.status(400).json({error})
+    }
+}
