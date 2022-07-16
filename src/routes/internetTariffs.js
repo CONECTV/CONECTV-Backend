739336deba2e64558c83d92b529842eda1143aff
RafaGomezGlez <A01733983@itesm.mx>
2022-07-16 17:17:41 -0500

    feat(internetTariffs): Delete endpoint CON-82

--- src/routes/internetTariffs.js
+++ src/routes/internetTariffs.js
@@ -2,11 +2,13 @@ const express = require('express');
 const createInternetTariff = require('../controllers/internetTariffs');
 const getInternetTariffs = require('../controllers/internetTariffs');
 const updateInternetTariff = require('../controllers/internetTariffs');
+const deleteInternetTariff = require('../controllers/internetTariffs');
 
 const router = express.Router();
 
 router.post('/crear', createInternetTariff.createInternetTariff);
 router.get('/obtener', getInternetTariffs.getInternetTariffs);
 router.put('/actualizar', updateInternetTariff.updateInternetTariff);
+router.delete('/eliminar', deleteInternetTariff.deleteInternetTariff);
 
 module.exports = router;
