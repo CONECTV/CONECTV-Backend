a81461064045a88a1bfe60255c2b8c45ff22f81a
RafaGomezGlez <A01733983@itesm.mx>
2022-07-16 17:25:25 -0500

    feat(availableAbono): delete endpoint CON-84

--- src/routes/availableAbonos.js
+++ src/routes/availableAbonos.js
@@ -2,11 +2,13 @@ const express = require('express');
 const createAvailableAbono = require('../controllers/availableAbonos');
 const getAvailableAbonos = require('../controllers/availableAbonos');
 const updateAvailableAbono = require('../controllers/availableAbonos');
+const deleteAvailableAbono = require('../controllers/availableAbonos');
 
 const router = express.Router();
 
 router.post('/crear', createAvailableAbono.createAvailableAbono);
 router.get('/obtener', getAvailableAbonos.getAvailableAbonos);
 router.put('/actualizar', updateAvailableAbono.updateAvailableAbono);
+router.delete('/eliminar', deleteAvailableAbono.deleteAvailableAbono);
 
 module.exports = router;
