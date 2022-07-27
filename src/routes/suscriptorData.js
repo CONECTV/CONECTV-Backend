7df1b2dea8524db0f7549f0c2ceb4555d12e213c
RafaGomezGlez <A01733983@itesm.mx>
2022-07-26 20:41:38 -0500

    ref(suscriptorData): Deleting endpoint for contract searching

--- src/routes/suscriptorData.js
+++ src/routes/suscriptorData.js
@@ -3,7 +3,6 @@ const createSuscriptorData = require('../controllers/suscriptorData');
 const getSuscriptorsData = require('../controllers/suscriptorData');
 const updateSuscriptorData = require('../controllers/suscriptorData');
 const getSuscriptorsDataSearchBar = require('../controllers/suscriptorData');
-const getSuscriptorData = require('../controllers/suscriptorData');
 
 const router = express.Router();
 
@@ -11,6 +10,5 @@ router.post('/crear', createSuscriptorData.createSuscriptorData);
 router.get('/obtener', getSuscriptorsData.getSuscriptorsData);
 router.put('/actualizar', updateSuscriptorData.updateSuscriptorData);
 router.post('/search', getSuscriptorsDataSearchBar.getSuscriptorsDataSearchBar);
-router.post('/searchContrato', getSuscriptorData.getSuscriptorData);
 
 module.exports = router;
